1176f8d6d3680d2101cd036d72ce2d82857c0a81
Ben van den Ende <ben@meedoenisbelangrijkerdanwinnen.nl>
2014-03-30 21:20:12 +0200

    Crunk aint dead

--- public/scripts/yossarian.js
+++ public/scripts/yossarian.js
@@ -1,26 +1,35 @@
 $('.header-nav a').on('click', function(event) {
 	event.preventDefault();
 	var href = $(this).attr('href');
+	var slug = $(this).parent('li').attr('id');
+	var name = $(this).attr('title');
 
-	$('#main').html('<div class="loading"><i class="fa fa-refresh fa-spin"></i></div>');
 	$('#main').load(href + ' #main > *');
 
 	$('.header-nav li').removeClass('active');
 	$(this).parent('li').addClass('active');
 
+	$('footer nav li').removeClass('active');
+	$('footer nav li#' + slug).addClass('active');
+
 	window.history.pushState({}, '', href);
+	document.title = 'Yossarian \u2014 ' + name;
 });
 
-$('.sidebar-nav a').on('click', function(event) {
+$('#main').on('click', '.sidebar-nav a', function(event) {
 	event.preventDefault();
 	var href = $(this).attr('href');
+	var name = $(this).attr('title');
 
-	$('#main').html('<div class="loading"><i class="fa fa-refresh fa-spin"></i></div>');
-	$('#main').load(href + ' #main > *');
+	$('#content').load(href + ' #content > *');
 
 	$('.sidebar-nav li').removeClass('active');
 	$(this).parent('li').addClass('active');
 
+	$(this).parent('li').find('.sidebar-subnav li:first').addClass("active");
+	$(this).parent('li').parent('.sidebar-subnav').parent('li').addClass('active');
+
 	window.history.pushState({}, '', href);
+	document.title = 'Yossarian \u2014 ' + name;
 });
 
