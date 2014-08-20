ba65d2fba3cd93deaf5336fa48027d8152075297
Ben van den Ende <ben@meedoenisbelangrijkerdanwinnen.nl>
2014-08-20 15:10:59 +0200

    Let's start fresh

--- app.js
+++ app.js
@@ -1,84 +1,81 @@
-// REQUIREMENTS
 var express = require('express');
 var less = require('less-middleware');
-var mongodb = require('mongodb');
-var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
+var mongodb = require('mongodb');
+var mongoose = require('mongoose');
 var moment = require('moment');
+
 var dateFormat = "ddd MM-DD-YYYY HH:mm:ss";
-
-var utils = require(__dirname + '/handlers/auth/utils');
 var User = require(__dirname + '/models/user');
-
-// DATABASE CONNECTION
-mongoose.connect('mongodb://localhost/yossarian');
+  
+mongoose.connect('mongodb://dagobert:g3ldm03tr0llen@localhost:23383/poen');
 mongoose.connection.on('error', console.error.bind(console, 'Connection error:'));
 mongoose.connection.once('open', function callback() {
-	console.log(moment().format(dateFormat) + ' - Connection to the Mongo database established. Ready for launch.');
+	console.log(moment().format(dateFormat) + ' - De database werkt. Wat een geluk');
 });
 
-// PASSPORT CONNECTION
 passport.serializeUser(function(user, done) {
-	done(null, user.email);
+	done(null, user.id);
 });
 
-passport.deserializeUser(function(email, done) {
-	User.findOne( { email: email } , function (err, user) {
+passport.deserializeUser(function(id, done) {
+	User.findById(id, function (err, user) {
 		done(err, user);
 	});
 });
 
-passport.use(new LocalStrategy(
-	function(username, password, done) {
-
-		User.findOne({ email: username }, function(err, user) {
-			if (err) { return done(err); }
-			if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-			
-			user.comparePassword(password, function(err, isMatch) {
-				if (err) return done(err);
-				if(isMatch) {
-					return done(null, user);
-				} else {
-					return done(null, false, { message: 'Invalid password' });
-				}
-			});
+passport.use(new LocalStrategy(function(username, password, done) {
+	User.findOne({ username: username }, function(err, user) {
+		if (err) { return done(err); }
+		if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+		user.comparePassword(password, function(err, isMatch) {
+			if (err) return done(err);
+			if(isMatch) {
+				return done(null, user);
+			} else {
+				return done(null, false, { message: 'Invalid password' });
+			}
 		});
-	}
-));
+	});
+}));
 
 var app = express();
 
-// MAIN CONFIGURATION
-app.configure(function (){ 
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
 
-	app.engine('html', require('ejs').renderFile);
-	app.set('views', __dirname + '/views');
+app.use(less(__dirname + '/', {
+	force: true,
+	dest: __dirname + '/public'
+}));
 
-	app.use(express.json());
-	app.use(express.urlencoded());
-	app.use(express.cookieParser('Where are the Snowdens of yesteryear?'));
-	app.use(express.methodOverride());
-	app.use(express.session());
 
-	app.use(passport.initialize());
-	app.use(passport.session());
+app.use(express.json());
 
-	app.use(app.router);
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'Dagobert Duck' }));
 
-	app.use(less(__dirname + '/', {
-		force: true,
-		dest: __dirname + '/public'
-	}));
-	app.use(express.static(__dirname + '/public')); 
+app.use(passport.initialize());
+app.use(passport.session());
 
-});
+app.use(app.router);
+app.use(express.static(__dirname + '/public')); 
 
-// ROUTES
 var routes = require(__dirname + '/handlers/routes')(app);
 
-app.listen(3000, function() {
-	console.log('\n----------\nYOSSARIAN\n----------\n\n'+ moment().format(dateFormat) + ' - Let\'s get this party started (http://localhost:3000)!');
+app.listen(11074, function() {
+	console.log('\n\nPOEN POEN POEN\n\n' + moment().format(dateFormat) + ' - Geld moet rollen');
 });
+
+/* SEED A USER 
+var user = new User({ username: 'admin', email: 'admin@example.com', password: 'secret' });
+user.save(function(err) {
+	if(err) {
+		console.log(err);
+	} else {
+		console.log(moment().format(dateFormat) + ' - New user seeded: ' + user.username + ".");
+	}
+}); */
--- models/categories.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var categorySchema = new Schema({
-	categories: [String]
-});
-
-module.exports = mongoose.model('Category', categorySchema);
